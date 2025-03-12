0305a520b6c21c4069d22c597cda89bb9332ec13
mobdico <mobdico59@gmail.com>
2025-03-12 11:36:07 +0100

    modification de APP.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
-import {useState, useEffect} from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Home from "./pages/Home";
 import MainPage from "./pages/MainPage";
+import Profile from "./pages/Profile";
 
 const App = () => {
-    const [login, setLogin] = useState(null)
+  const [login, setLogin] = useState(null);
 
-    useEffect(() => {
-        const token = localStorage.getItem("token")
-        setLogin(Boolean(token))
-    })
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setLogin(Boolean(token));
+  }, []);
 
+  if (login === null) {
+    return <div>Chargement...</div>;
+  }
 
-    if (!login) {
-        return <Home/>
-    } else
-        return (
-            <Router>
-                <Routes>
-                    <Route path="/" element={<MainPage/>}/>
-
-                </Routes>
-            </Router>)
-
+  return (
+    <Routes>
+      <Route path="/" element={!login ? <Home setLogin={setLogin} /> : <Navigate to="/home" replace />} />
+      <Route path="/home" element={login ? <MainPage /> : <Navigate to="/" replace />} />
+      <Route path="/profile" element={login ? <Profile /> : <Navigate to="/" replace />} />
+    </Routes>
+  );
 };
 
 export default App;
+
+
