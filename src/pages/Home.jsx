c04756d2f57528a4943101a1e8af3190cf496930
stevietchinda <stevietchinda215@gmail.com>
2025-03-12 14:17:08 +0100

    home conflit resolution

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,28 +23,11 @@ const Home = () => {
     })
 
     return (
-<<<<<<< HEAD
-        <div className="home-container">
-            <div className="left-section">
-                <img src="/logo-x.png" alt="Logo X" className="logo" />
-            </div>
-            <div className="right-section">
-                <h1>Ça se passe maintenant !</h1>
-                <p>{isLogin ? "Connectez-vous" : "Inscrivez-vous"}</p>
-                {isLogin ? <Login /> : <Signup />}
-                <p className="toggle-auth">
-                    {isLogin ? "Vous n'avez pas de compte ?" : "Vous avez déjà un compte ?"}
-                    <span onClick={() => setIsLogin(!isLogin)}>
-                        {isLogin ? "S'inscrire" : "Se connecter"}
-                    </span>
-                </p>
-=======
         <div className="flex flex-col h-full">
             <TweetInput onTweet={submitTweet}/>
             <div className="flex flex-col flex-1 overflow-y-auto">
                 {tweets.map((tweet) => <Tweet key={tweet.id} {...tweet} toggleFavorite={toggleFavorite}/>
                 )}
->>>>>>> 3de52dd0a85453a9e56c9984aea6e2d0f383edec
             </div>
         </div>
     );
